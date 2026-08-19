643d271ac96bfd67b6e7a418bf366505f05aee6d
laith alardah <laithalardah05@gmail.com>
2026-08-19 12:27:45 +0300

    refactor : removed unused imports

--- PaymentService/src/main/java/com/example/paymentservice/exception/TransferNotFoundException.java
+++ PaymentService/src/main/java/com/example/paymentservice/exception/TransferNotFoundException.java
@@ -1,6 +1,5 @@
 package com.example.paymentservice.exception;
 
-import com.example.paymentservice.service.TransactionEventService;
 
 public class TransferNotFoundException extends RuntimeException{
     public TransferNotFoundException(String message) {
